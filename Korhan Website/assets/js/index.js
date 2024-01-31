d42d9a5ae4d6d2f9a409c208dee02d52ffde4230
Fuczcu <158353288+Fuczcu@users.noreply.github.com>
2024-01-31 21:59:40 +0100

    Delete Korhan Website directory

--- Korhan Website/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-
-window.addEventListener("scroll", function()
-{
-    if(window.innerHeight > window.scrollY) {
-        document.getElementById("bgImg").style.filter = "blur("+(window.scrollY/window.innerHeight*35)+"px)";
-    }
-});
