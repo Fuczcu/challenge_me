05de5d802f52e0bf89463a8d48be577138357e52
Fuczcu <158353288+Fuczcu@users.noreply.github.com>
2024-01-31 22:31:32 +0100

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 
 window.addEventListener("scroll", function()
 {
+    const blurPx = 20;
     if(window.innerHeight > window.scrollY) {
-        document.getElementById("bgImg").style.filter = "blur("+(window.scrollY/window.innerHeight*35)+"px)";
+        document.getElementById("bgImg").style.filter = "blur("+(window.scrollY/window.innerHeight*blurPx)+"px)";
     }
 });
